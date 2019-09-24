88b319efc891048a6ab4c895d7f73638ff3c6746
tylerz1443 <42786943+tylerz1443@users.noreply.github.com>
2019-09-23 23:34:27 -1000

    1

---
+++ ziplist.js
@@ -0,0 +1,23 @@
+function zipList(list1, list2) {
+
+  const final = [];
+  let x = 0;
+
+  for(let i = 0; i < list1.length; i++) {
+
+    final[x] = list1[i];
+    x++;
+    final[x] = list1[i];
+    x++;
+
+  }
+
+  return final;
+
+}
+
+function ziplistTheSimpleWay(list1, list2) {
+
+  return _.zip(list1, list2);
+
+}
