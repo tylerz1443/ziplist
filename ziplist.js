d3a580ca30361f128f66038d3b545163fe758e30
tylerz1443 <42786943+tylerz1443@users.noreply.github.com>
2019-09-23 23:37:21 -1000

    small change

--- ziplist.js
+++ ziplist.js
@@ -1,23 +1,15 @@
 function zipList(list1, list2) {
-
   const final = [];
   let x = 0;
-
-  for(let i = 0; i < list1.length; i++) {
-
+  for (let i = 0; i < list1.length; i++) {
     final[x] = list1[i];
     x++;
-    final[x] = list1[i];
+    final[x] = list2[i];
     x++;
-
   }
-
   return final;
-
 }
 
 function ziplistTheSimpleWay(list1, list2) {
-
   return _.zip(list1, list2);
-
 }
